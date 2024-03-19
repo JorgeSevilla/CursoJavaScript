fe799d6f706a421d26e519929cb7b79a491d0248
Jorge <jorge.garcia@tcesc.tc.br>
2024-03-19 08:20:08 -0300

    Tipo de dato primitivo: number

---
+++ number.js
@@ -0,0 +1,35 @@
+// Tipo entero y decimal
+const entero = 10;
+const decimal = 12.5;
+console.log(typeof entero, typeof decimal);
+
+//Notación científica
+const cientifica = 5e3;
+
+//Infinitos y NaN
+const infinito = Infinity;
+const noEsNumero = NaN;
+
+//Operaciones aritméticas
+const suma = 1 + 2;
+const resta = 2 - 1;
+const multiplicacion = 2 * 2;
+const division = 10 / 2;
+
+//Módulo y Potencia
+const modulo = 5 % 2;
+const potencia = 2 ** 4;
+
+//Precisión
+const precision = 0.1 + 0.2;
+console.log(precision)
+console.log(precision.toFixed(1));
+console.log(precision === 0.3);
+
+//Operaciones avanzadas
+const raizCuadrada = Math.sqrt(16);
+const valorAbsoluto = Math.abs(-7);
+const aleatorio = Math.random();
+console.log(raizCuadrada);
+console.log(valorAbsoluto); 
+console.log(aleatorio); 
