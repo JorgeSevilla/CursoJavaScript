ba1f973c2f49089bf3a276201eb9360aece5b18a
Jorge <jorge.garcia@tcesc.tc.br>
2024-03-20 08:25:10 -0300

    Conversión a Booleanos

---
+++ boolean.js
@@ -0,0 +1,14 @@
+const isActive = true;
+const hasPrmission = false;
+
+//Conversión implícita
+const result = 5 > 3;
+console.log(result);
+const name = 'Jorge';
+console.log(!!name);
+
+//Conversión explícita
+const value = 0;
+const otherValue = -24;
+const explicitBoolean = Boolean(otherValue);
+console.log(explicitBoolean);
